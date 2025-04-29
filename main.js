cf8e8cb94de098cf715c80867b9b424126494d3c
Francoistlb <113359189+Francoistlb@users.noreply.github.com>
2025-04-29 10:24:16 +0200

    test4

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-export function add(numbers) {
+export function add(numbers, delimiter) {
+
     if (numbers === "") {
         return 0
+    }
+
+    if (numbers.includes('[') 
+        && numbers.includes(']') 
+        && delimiter.includes('[') 
+        && delimiter.includes(']')) {
+
+        throw new Error('[] ne sont pas permis')
     }
 
     return numbers
--- main.test.js
+++ main.test.js
@@ -9,16 +9,24 @@ test('add 1,2 to equal 3', () => {
   expect(add("1,2")).toBe(3)
 })
 
+//1
 test('empty string returns 0', () => {
   expect(add("")).toBe(0)
 })
 
+//2
 test('add 10,30 to equal 40', () => {
   expect(add("10,30")).toBe(40)
 })
 
+//3
 test('add 1\n2,3 to equal 6', () => { 
   expect(add("1\n2,3")).toBe(6)
+})
+
+//4
+test('//[delimiter]\n[numbers]', () => {
+  expect(add("//;\n1;2")).toBe(3)
 })
 
 
