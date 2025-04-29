294de42eb963453edcf8cc47789f4a6122e7f8f7
Francoistlb <113359189+Francoistlb@users.noreply.github.com>
2025-04-29 10:27:46 +0200

    test4_refactor

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
-export function add(numbers, delimiter) {
-
+export function add(numbers) {
     if (numbers === "") {
         return 0
     }
 
-    if (numbers.includes('[') 
-        && numbers.includes(']') 
-        && delimiter.includes('[') 
-        && delimiter.includes(']')) {
-
+    if (numbers.includes('[]')) {
         throw new Error('[] ne sont pas permis')
     }
 
-    return numbers
-        .split(/[,\n]/)
+    let delimiter = ',';
+    let numbersToProcess = numbers;
+
+    
+    if (numbers.startsWith('//')) {
+        delimiter = numbers[2];
+        numbersToProcess = numbers.substring(numbers.indexOf('\n') + 1);
+    }
+
+    return numbersToProcess
+        .split(new RegExp(`[${delimiter}\n,]`))
         .map(Number)
         .reduce((accum, numactuel) => accum + numactuel, 0)
 }
